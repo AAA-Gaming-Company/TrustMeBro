4e37dcf96b38dd6d88e4da3258766058c822daf4
unknown <CharlesVeryGudBoy999@users.noreply.github.com>
2025-02-19 16:21:59 +0100

    USELESS CAMERA CHANGES AAAHAH

--- Assets/Scripts/World/Triggers/CameraTriggerArea.cs
+++ Assets/Scripts/World/Triggers/CameraTriggerArea.cs
@@ -1,6 +1,28 @@
+using System.Collections;
+using Unity.Cinemachine;
 using UnityEngine;
 
-public class CameraTrigger
-{
-    
+public class CameraTriggerArea : TriggerArea {
+    [Header("Camera")]
+    public CinemachineCamera cinemachineCamera;
+    public float targetOffsetY = 0f;
+    public float orthographicSize = 0f;
+
+    protected override void TriggerAction() {
+        CinemachinePositionComposer positionComposer = cinemachineCamera.GetComponent<CinemachinePositionComposer>();
+        if (positionComposer == null) {
+            throw new System.Exception("CinemachinePositionComposer is null!");
+        }
+        
+        positionComposer.Composition.DeadZone.Size.y = 0;
+
+        cinemachineCamera.Lens.OrthographicSize = orthographicSize;
+        positionComposer.TargetOffset.y = targetOffsetY;
+        StartCoroutine(EnableDeadZone());
+    }
+
+    private IEnumerator EnableDeadZone() {
+        yield return new WaitForSeconds(.01f);
+        cinemachineCamera.GetComponent<CinemachinePositionComposer>().Composition.DeadZone.Size.y = 1;
+    }
 }
