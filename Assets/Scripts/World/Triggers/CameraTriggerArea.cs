847f8918b5811a232963599b43da5daf1d2bb31f
ImDaBigBoss <67973871+ImDaBigBoss@users.noreply.github.com>
2025-02-22 20:14:38 +0100

    Camera magic?

--- Assets/Scripts/World/Triggers/CameraTriggerArea.cs
+++ Assets/Scripts/World/Triggers/CameraTriggerArea.cs
@@ -12,17 +12,39 @@ public class CameraTriggerArea : TriggerArea {
 
     public bool deadZone = true;
 
+    public float duration = 0f;
+
+    private CinemachinePositionComposer positionComposer;
+
     protected override void TriggerAction(PlayerController player) {
-        CinemachinePositionComposer positionComposer = cinemachineCamera.GetComponent<CinemachinePositionComposer>();
-        if (positionComposer == null) {
+        this.positionComposer = cinemachineCamera.GetComponent<CinemachinePositionComposer>();
+        if (this.positionComposer == null) {
             throw new System.Exception("CinemachinePositionComposer is null!");
         }
-        
-        positionComposer.Composition.DeadZone.Enabled = deadZone;
-        cinemachineCamera.Lens.OrthographicSize = orthographicSize;
-        positionComposer.Composition.ScreenPosition.y = targetOffsetY;
-        positionComposer.Composition.ScreenPosition.x = targetOffsetX;
 
+        if (duration == 0f) {
+            this.cinemachineCamera.Lens.OrthographicSize = this.orthographicSize;
+            this.positionComposer.Composition.DeadZone.Enabled = this.deadZone;
+            this.positionComposer.Composition.ScreenPosition.y = this.targetOffsetY;
+            this.positionComposer.Composition.ScreenPosition.x = this.targetOffsetX;
+        } else {
+            StartCoroutine(this.ChangeCamera());
+        }
     }
 
+    private IEnumerator ChangeCamera() {
+        float elapsedTime = 0f;
+        float startOrthographicSize = this.cinemachineCamera.Lens.OrthographicSize;
+        float startTargetOffsetY = this.positionComposer.Composition.ScreenPosition.y;
+        float startTargetOffsetX = this.positionComposer.Composition.ScreenPosition.x;
+
+        while (elapsedTime < duration) {
+            this.cinemachineCamera.Lens.OrthographicSize = Mathf.Lerp(startOrthographicSize, this.orthographicSize, elapsedTime / this.duration);
+            this.positionComposer.Composition.ScreenPosition.y = Mathf.Lerp(startTargetOffsetY, this.targetOffsetY, elapsedTime / this.duration);
+            this.positionComposer.Composition.ScreenPosition.x = Mathf.Lerp(startTargetOffsetX, this.targetOffsetX, elapsedTime / this.duration);
+
+            elapsedTime += Time.deltaTime;
+            yield return null;
+        }
+    }
 }
