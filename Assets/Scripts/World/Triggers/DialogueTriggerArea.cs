d82bd619c818c4bfd575f95b965046f5e4d9fe85
unknown <CharlesVeryGudBoy999@users.noreply.github.com>
2025-02-23 02:04:58 +0100

    Added Mandrew positions. Last commit for me :D

--- Assets/Scripts/World/Triggers/DialogueTriggerArea.cs
+++ Assets/Scripts/World/Triggers/DialogueTriggerArea.cs
@@ -3,8 +3,14 @@ using UnityEngine;
 public class DialogueTriggerArea : TriggerArea {
     [Header("Dialogue")]
     public DialogueEntry[] entries;
+    public bool teleportMandrew = true;
+    public Transform mandrewTeleportTarget;
+    public Transform mandrewTransform;
 
     protected override void TriggerAction(PlayerController player) {
+        if (this.teleportMandrew) {
+            this.mandrewTransform.position = this.mandrewTeleportTarget.position;
+        }
         DialogueBuilder.Builder()
             .AddEntry(this.entries)
             .BuildAndDisplay();
